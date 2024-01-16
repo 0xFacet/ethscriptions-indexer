6a52bec00a6e7bc81f0a40513ea3732f208b7c5e
Tom Lehman <tom.s.lehman@gmail.com>
2024-01-16 15:51:13 -0500

    Ability to filter transfers by transaction hash

--- app/controllers/ethscription_transfers_controller.rb
+++ app/controllers/ethscription_transfers_controller.rb
@@ -5,8 +5,9 @@ class EthscriptionTransfersController < ApplicationController
     
     scope = EthscriptionTransfer.all.page(page).per(per_page)
     
-    scope = scope.where(from_address: params[:from].downcase) if params[:from].present?
-    scope = scope.where(to_address: params[:to].downcase) if params[:to].present?
+    scope = scope.where(from_address: parse_param_array(params[:from])) if params[:from].present?
+    scope = scope.where(to_address: parse_param_array(params[:to])) if params[:to].present?
+    scope = scope.where(transaction_hash: parse_param_array(params[:transaction_hash])) if params[:transaction_hash].present?
     
     scope = params[:sort_order]&.downcase == "asc" ? scope.oldest_first : scope.newest_first
     

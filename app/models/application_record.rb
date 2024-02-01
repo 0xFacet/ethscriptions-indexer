53e7434f2ba4198e1f283300efd540d242263fd7
Tom Lehman <tom.s.lehman@gmail.com>
2024-02-01 16:38:24 -0500

    Multi-db setup

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  
+  if Rails.env.production? && ENV['FOLLOWER_DATABASE_URL'].present?
+    connects_to database: { writing: :primary, reading: :primary_replica }
+  end
 end

f7a348c333ab31725d4a0743fd01efc478695bf3
Tom Lehman <tom.s.lehman@gmail.com>
2024-02-02 17:21:32 -0500

    Restructure db config

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
   
-  if ENV['REPLICA_DATABASE_URL'].present?
-    connects_to database: { writing: :primary, reading: :primary_replica }
-  end
+  connects_to database: { writing: :primary, reading: :primary_replica }
 end

1b9fc0b5b3651e77c03334849d3c8971c7abd732
Tom Lehman <tom.s.lehman@gmail.com>
2024-02-03 10:40:28 -0500

    Clean up database setup

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,15 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
   
-  connects_to database: { writing: :primary, reading: :primary_replica }
+  def self.use_replica_database?
+    Rails.env.test? ?
+      ENV['TEST_REPLICA_DATABASE_URL'].present? :
+      ENV['REPLICA_DATABASE_URL'].present?
+  end
+  
+  if use_replica_database?
+    connects_to database: { writing: :primary, reading: :primary_replica }
+  else
+    connects_to database: { writing: :primary }
+  end
 end
